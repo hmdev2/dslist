23bb639ea2dc1acb3f1fa0c07b409aab1ada6cca
Pedro Melo <hmdev2@proton.me>
2025-05-20 12:31:22 -0300

    Game repository, service, controller

---
+++ src/main/java/com/peumeloaluno/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.peumeloaluno.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.peumeloaluno.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
